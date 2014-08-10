e5f9892a19c7ef2ddd784122768be799d4dfd328
Brendan Schwartz <brendanschwartz@gmail.com>
2014-08-10 11:33:54 -0400

    Only install to_nsq if we're using 0.2.30 or greater

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,2 @@
-* 1.2.1 - Use NSQ 0.2.30 and Go 1.3 by default. Added support for `to_nsq` utility.
+* 1.1.2 - Added support for the `to_nsq` utility when installing 0.2.30 and above.
 * 1.0.0 - Initial Release
--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 # encoding: UTF-8
 
 # Versions
-default['nsq']['version'] = '0.2.30'
-default['nsq']['go_version'] = 'go1.3'
+default['nsq']['version'] = '0.2.26'
+default['nsq']['go_version'] = 'go1.2'
 
 # Architecture
 default['nsq']['arch'] = 'linux-amd64'
