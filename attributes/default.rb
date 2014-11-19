6135eccec29d30523375d5c261d65853ab85fc12
Matt Reiferson <mreiferson@gmail.com>
2014-11-19 10:26:34 -0800

    bump default version to 0.3.0 go1.3.3

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 # encoding: UTF-8
 
 # Versions
-default['nsq']['version'] = '0.2.30'
-default['nsq']['go_version'] = 'go1.3'
+default['nsq']['version'] = '0.3.0'
+default['nsq']['go_version'] = 'go1.3.3'
 
 # Architecture
 default['nsq']['arch'] = 'linux-amd64'
